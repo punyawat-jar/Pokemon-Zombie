6f7b8686788db11048ef4a11745b8109c87fe35e
Podolph <nalin_hippo@hotmail.com>
2021-12-09 21:14:00 +0700

    Refined TimeWait in ZombieThread

--- src/main/java/Keybaord_bar.java
+++ src/main/java/Keybaord_bar.java
@@ -15,7 +15,8 @@ class Keyboard_bar {
     private int width, height;
     private MainApplication main;
     private ArrayList<Wordbox> word_AL;
-    private String a,b;
+    private String a, b;
+
     public Keyboard_bar(ArrayList<Wordbox> wAL) {
         word_AL = wAL;
         typearea = new JTextArea();
@@ -25,33 +26,33 @@ class Keyboard_bar {
         typearea.addKeyListener(new KeyListener() {
             public void keyPressed(KeyEvent e) {
                 if (e.getKeyCode() == KeyEvent.VK_SPACE) {
-                    //if(typearea.getText().trim().contains(word_AL.get(main.getCount_death()).getWord().trim())){
-                        
-                        //main.kill_monster(main.threadlist.get(main.getCount_death()));
+                    // if(typearea.getText().trim().contains(word_AL.get(main.getCount_death()).getWord().trim())){
 
-                    //}
+                    // main.kill_monster(main.threadlist.get(main.getCount_death()));
+
+                    // }
                     // a = typearea.getText().trim();
                     // b = word_AL.get(main.threadlist.get(main.getCount_death())).getWord().trim();
                     // System.out.println("-" + a + "-");
                     // System.out.println("*" + b + "*");
                     // System.out.println("death count = " + main.getCount_death() );
                     // for(int i =0;i<10;i++){
-                    //     System.out.println("WordAL = " + word_AL.get(i).getWord());
+                    // System.out.println("WordAL = " + word_AL.get(i).getWord());
                     // }
-                    if(typearea.getText().trim().equals(word_AL.get(main.threadlist.get(main.getCount_death())).getWord().trim())){
-                        ///score++
+                    if (typearea.getText().trim()
+                            .equals(word_AL.get(main.threadlist.get(main.getCount_death())).getWord().trim())) {
+                        /// score++
                         main.kill_zombie(main.threadlist.get(main.getCount_death()));
                         System.out.println("count is = " + main.getCount_death());
                         System.out.println("thread is = " + main.threadlist);
                         System.out.println("mY name is your");
                     }
                     typearea.setText(null);
-                    
+
                 }
-                if (e.getKeyCode() == KeyEvent.VK_ESCAPE){
+                if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
                     main.pause();
                 }
-
             }
 
             public void keyTyped(KeyEvent e) {
@@ -61,37 +62,33 @@ class Keyboard_bar {
             }
         });
     }
-    
+
     public JTextArea getTypearea() {
         return typearea;
     }
 
-    public void setPane(JLabel x,MainApplication m) {
+    public void setPane(JLabel x, MainApplication m) {
         main = m;
         x.add(typearea);
     }
-
 
     public void setposition(int x, int y) {
         typearea.setBounds(x, y, width, height);
     }
 }
 
-
-
-class Compare_text implements CaretListener{
+class Compare_text implements CaretListener {
     private JTextArea in;
     private Wordbox out;
 
-    public Compare_text(JTextArea p, Wordbox t){ 
-	in = p;
-    out = t;
-	in.addCaretListener( this );
+    public Compare_text(JTextArea p, Wordbox t) {
+        in = p;
+        out = t;
+        in.addCaretListener(this);
     }
 
-    
     public void caretUpdate(CaretEvent e) {
 
-        //out.setText( in.getText() );
+        // out.setText( in.getText() );
     }
 };
