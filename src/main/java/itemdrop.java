a8faa937820f80a46a1d7a24627ab398aca9e4a0
punyawt <punyawat0@gmail.com>
2021-12-13 16:46:40 +0700

    Block item from appear in corner

--- src/main/java/itemdrop.java
+++ src/main/java/itemdrop.java
@@ -14,6 +14,7 @@ public class itemdrop extends Thread {
     JLabel pane;
     MainApplication main;
     boolean end = false, floor = false;
+    Thread item;
     ArrayList<JLabel> Item_AL = new ArrayList<JLabel>();
 
     public itemdrop(JLabel p, MainApplication m, ArrayList<JLabel> iAL) {
@@ -25,26 +26,33 @@ public class itemdrop extends Thread {
     public void run() {
         while (!end) {
             int rand_time = (int) Math.floor(Math.random() * (25000 - 18000 + 1) + 18000);
-            floor = false;
+            
             try {
-                Thread.sleep(rand_time); /// 1000 for debug only, use rand_time
+                Thread.sleep(10000); /// 10000 for debug only, use rand_time
             } catch (Exception e) {
             }
-            Thread item = new Thread() {
+            item = new Thread() {
                 public void run() {
-                    int CurX = (int) Math.floor(Math.random() * ((main.get_framewidth() - 100) - 100 + 1) + 100);
+                    int CurX = (int) Math.floor(Math.random() * ((main.get_framewidth() - 200) - 450 + 1) + 450);
                     int CurY = 50;
                     int rand_item = (int) Math.floor(Math.random() * (3 - 0 + 1) + 0);
-                    pane.add(Item_AL.get(rand_item)); /// Note change 0 to rand_item
+                    System.out.println("CurX is ==================== " + CurX);
+                    pane.add(Item_AL.get(rand_item));
+                    Item_AL.get(rand_item).setVisible(true);
+                    floor = false;
                     while (!floor) {
                         Item_AL.get(rand_item).setBounds(CurX, CurY, 100, 100);
                         if (CurY < main.get_frameheight() - 150) { // Move item down
+                            System.out.println("CurY is == " + CurY + "frameheight()-150 ==" + (main.get_frameheight() - 150));
                             CurY += 10;
+                            pane.repaint();
+                            pane.validate();
                         } else {
                             floor = true; // item on the floor
                             pane.remove(Item_AL.get(rand_item));
                         }
                         pane.repaint();
+                        pane.validate();
                         try {
                             Thread.sleep(100);
                         } catch (Exception e) {
@@ -59,7 +67,12 @@ public class itemdrop extends Thread {
         }
     }
 
+    public void setMouseinteracted(boolean x){
+        floor = x;
+    }
+
     public void setEndgame(boolean e) {
+        item.interrupt();
         end = e;
     }
 }
